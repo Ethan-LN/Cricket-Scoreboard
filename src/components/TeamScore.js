14c4ebe703826ee25d1e1cbfa316739532b5185a
Ethan <et.l.nicholas@gmail.com>
2023-07-19 16:08:49 +1000

    when wickets reached 10, remove buttons and display all out

--- src/components/TeamScore.js
+++ src/components/TeamScore.js
@@ -20,10 +20,11 @@ export const TeamScore = () => {
       <div>
         <div>Runs scores are:{runs}</div>
         <div>Wickets are:{wickets}</div>
+        {wickets===10? <div>ALl OUT</div> : <div>
         <button onClick={addOneRun}>1 Run</button>
         <button onClick={addFourRun}>4 Runs</button>
         <button onClick={addSixRun}>6 Runs</button>
-        <button onClick={addOneWicket}>Wicket</button>
+        <button onClick={addOneWicket}>Wicket</button> </div>}
       </div>
     </div>
   );
