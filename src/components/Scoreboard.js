5b7599be913410cb60507cad2ecd9761a6d89aa1
Ethan <et.l.nicholas@gmail.com>
2023-07-19 16:59:50 +1000

    add input box for name imput

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { TeamScore } from "./TeamScore";
 import "../styles/Scoreboard.css";
 
 export const Scoreboard = () => {
-  const teamOneName = "Cricket Time";
-  const temaTwoTeam = "Go Criecket";
+  const [teamOneName, setTeamOneName] = useState("");
+  const [teamTwoName, setTeamTwoName] = useState("");
+
+  const handleTeam1InputChange = (event) => {
+    setTeamOneName(event.target.value);
+  };
+
+  const handleTeam2InputChange = (event) => {
+    setTeamTwoName(event.target.value);
+  };
+
   return (
     <div className="scoreboard">
-      <TeamScore teamName={teamOneName} />
-      <TeamScore teamName={temaTwoTeam} />
+      <div>
+        <h2>Team 1</h2>
+        <input
+          type="text"
+          value={teamOneName}
+          onChange={handleTeam1InputChange}
+        />
+        <TeamScore teamName={teamOneName} />
+      </div>
+      <div>
+        <h2>Team 2</h2>
+        <input
+          type="text"
+          value={teamTwoName}
+          onChange={handleTeam2InputChange}
+        />
+        <TeamScore teamName={teamTwoName} />
+      </div>
     </div>
   );
 };
--- src/components/TeamScore.js
+++ src/components/TeamScore.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "../styles/TeamScore.css";
 export const TeamScore = (props) => {
   const [runs, setRuns] = useState(0);
   const [wickets, setWickets] = useState(0);
@@ -16,22 +16,20 @@ export const TeamScore = (props) => {
     setWickets(wickets + 1);
   };
   return (
-    <div>
-      <div>
-        <div>Team : {props.teamName}</div>
-        <div>Runs scores are:{runs}</div>
-        <div>Wickets are:{wickets}</div>
-        {wickets === 10 ? (
-          <div>ALl OUT</div>
-        ) : (
-          <div>
-            <button onClick={addOneRun}>1 Run</button>
-            <button onClick={addFourRun}>4 Runs</button>
-            <button onClick={addSixRun}>6 Runs</button>
-            <button onClick={addOneWicket}>Wicket</button>{" "}
-          </div>
-        )}
-      </div>
+    <div className="teamScore__content">
+      <h2>{props.teamName}</h2>
+      <div>Runs scores are:{runs}</div>
+      <div>Wickets are:{wickets}</div>
+      {wickets === 10 ? (
+        <div>ALl OUT</div>
+      ) : (
+        <div>
+          <button onClick={addOneRun}>1 Run</button>
+          <button onClick={addFourRun}>4 Runs</button>
+          <button onClick={addSixRun}>6 Runs</button>
+          <button onClick={addOneWicket}>Wicket</button>
+        </div>
+      )}
     </div>
   );
 };
